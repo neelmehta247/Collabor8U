af416752e24d2b33b19fb6aad7b11c30951c9ad6
Neel Mehta <neelmehta247@gmail.com>
2017-03-04 18:51:11 -0500

    Update localhost and acces_token call

--- Frontend/collabor8u/src/Home.js
+++ Frontend/collabor8u/src/Home.js
@@ -1,34 +1,34 @@
-import React from 'react';
-import cookie from 'react-cookie';
-import $ from 'jquery';
+import React from "react";
+import cookie from "react-cookie";
+import $ from "jquery";
 
 class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    let url = "https://api.myjson.com/bins";
-    let accessToken = cookie.load("accessToken");
-    this.state = {
-      session_object: {},
-    };
+    constructor(props) {
+        super(props);
+        let url = "http://localhost:5000";
+        let accessToken = cookie.load("accessToken");
+        this.state = {
+            session_object: {},
+        };
 
-    $.ajax({
-      type:"POST",
-      url: url,
-      data:'{"access_token":"' + accessToken + '"}',
-      dataType: "json",
-    contentType:"application/json; charset=utf-8",
-    }).done((data) => {
-      this.setState({session_object: data})
-    });
-  }
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: '{"facebook_access_token":"' + accessToken + '"}',
+            dataType: "json",
+            contentType: "application/json; charset=utf-8",
+        }).done((data) => {
+            this.setState({session_object: data})
+        });
+    }
 
-  render() {
-    return (
-      <div>
-      {this.state.session_object.suh}
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                {this.state.session_object.suh}
+            </div>
+        );
+    }
 }
 
 export default Home;
