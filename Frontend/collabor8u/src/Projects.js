1ff120c06e38a105b2ddec7abd488755eb4b1cfd
Bonnie Xu <bonnie.xuu@gmail.com>
2017-03-04 18:32:29 -0500

    Started second screen

---
+++ Frontend/collabor8u/src/Projects.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+import './Projects.css';
+
+
+
+class Projects extends React.Component {
+    render() {
+    return <button className="NewProject" /* onClick={activateLasers} */ >
+                Activate Lasers
+            </button>;
+    }
+}
+
+class Form extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {inputList: []};
+        this.onAddBtnClick = this.onAddBtnClick.bind(this);
+    }
+
+    onAddBtnClick(event) {
+        const inputList = this.state.inputList;
+        this.setState({
+            inputList: inputList.concat(<Projects key={inputList.length} />)
+        });
+    }
+
+    render() {
+        return (
+            <div className="Main">
+                <button className="addBtn" onClick={this.onAddBtnClick}>Click to add a Project</button>
+                {this.state.inputList}
+            </div>
+        );
+    }
+}
+
+
+
+
+export default Form;
