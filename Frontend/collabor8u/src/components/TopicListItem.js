97c0bd3297ff6c17b530fe17251a56626acd70b7
Rahul Dominic <rahulbdominic@gmail.com>
2017-03-05 15:00:31 -0500

    Revert "no message"
    This reverts commit d8c8e64e2fb582e61e6a6aee0158323d9dc69f29.

--- Frontend/collabor8u/src/components/TopicListItem.js
+++ Frontend/collabor8u/src/components/TopicListItem.js
@@ -4,17 +4,15 @@ import ".././Notes.css";
 export default class TopicListItem extends React.Component {
     constructor(props) {
         super(props);
-        console.log(this.props.dataKey);
+
         this.state = {
-            isEditing: false,
-            dataKey: this.props.dataKey,
-            title: this.props.title,
+            isEditing: false
         };
     }
 
     render() {
         return (
-            <div className="TopicLst" data-key={this.props.dataKey} onClick={this.props.onClickHandler}>
+            <div className="TopicLst">
                 {this.props.title}
             </div>
         );
