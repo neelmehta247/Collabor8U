9ba43c7fef05f2c103a1d888e0f253d99b10d0a2
Bonnie Xu <bonnie.xuu@gmail.com>
2017-03-05 02:11:05 -0500

    Created new screen

---
+++ Frontend/collabor8u/src/components/app.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import CreateTodo from './create-todo.js';
+import TodosList from './todos-list.js';
+import '.././Notes.css'
+
+const todos = [
+{
+    task: 'make React tutorial',
+    isCompleted: false
+},
+{
+    task: 'eat dinner',
+    isCompleted: true
+}
+];
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            todos
+        };
+    }
+
+    render() {
+        return (
+            <div className="NotesMain">
+                <div className="TopicHeader">Topics</div>
+                <CreateTodo todos={this.state.todos} createTask={this.createTask.bind(this)} />
+                <TodosList
+                    todos={this.state.todos}
+                    toggleTask={this.toggleTask.bind(this)}
+                    saveTask={this.saveTask.bind(this)}
+                    deleteTask={this.deleteTask.bind(this)}
+                />
+            </div>
+        );
+    }
+
+    toggleTask(task) {
+        const foundTodo = find(this.state.todos, todo => todo.task === task);
+        foundTodo.isCompleted = !foundTodo.isCompleted;
+        this.setState({ todos: this.state.todos });
+    }
+
+    createTask(task) {
+        this.state.todos.push({
+            task,
+            isCompleted: false
+        });
+        this.setState({ todos: this.state.todos });
+    }
+
+    saveTask(oldTask, newTask) {
+        const foundTodo = find(this.state.todos, todo => todo.task === oldTask);
+        foundTodo.task = newTask;
+        this.setState({ todos: this.state.todos });
+    }
+
+    deleteTask(taskToDelete) {
+        delete(this.state.todos, todo => todo.task === taskToDelete);
+        this.setState({ todos: this.state.todos });
+    }
+}
--- Frontend/collabor8u/src/index.js
+++ Frontend/collabor8u/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import LoginPage from "./Pages/LoginPage";
 import HomePage from "./Pages/HomePage";
 import NotebookPage from "./Pages/NotebookPage";
+import Notes from "./Notes";
 import "./index.css";
 import {Router, Route, browserHistory} from "react-router";
 
@@ -11,6 +12,7 @@ ReactDOM.render(
         <Route path="/" component={LoginPage}/>
         <Route path="/home/" component={HomePage}/>
         <Route path="/notebook/:notebookId" component={NotebookPage}/>
+        <Route path="/notes/" component={Notes}/>
     </Router>,
     document.getElementById('root')
 );
